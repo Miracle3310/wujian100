012c94d6af3c77da64cdda1f699bce0724ced459
miracle3310 <miracle3310@qq.com>
2021-07-06 15:54:09 +0800

    spi test

--- sdk/projects/examples/driver/spi/example_spi.c
+++ sdk/projects/examples/driver/spi/example_spi.c
@@ -20,8 +20,8 @@
 #define OPERATE_ADDR    0x0
 #define OPERATE_LEN     256
 #define SPIFLASH_BASE_VALUE 0x0
-#define MY_USI_IDX 1 //select US0
-#define MY_SPI_CLK_RATE 10000
+#define MY_USI_IDX 1 //select USI1
+#define MY_SPI_CLK_RATE 100000
 #define TEST_SPI_TIMEOUT 500
 
 extern int32_t w25q64flash_read_id(spi_handle_t handle, uint32_t* id_num);
@@ -31,11 +31,11 @@ extern int32_t w25q64flash_program_data(spi_handle_t handle, uint32_t addr, cons
 extern int32_t w25q64flash_read_data(spi_handle_t handle, uint32_t addr, void* data, uint32_t cnt);
 extern int32_t drv_pinmux_config(pin_name_e pin, pin_func_e pin_func);
 
-spi_handle_t spi_t;
+static spi_handle_t spi_t;
 
 static void spi_event_cb_fun(int32_t idx, spi_event_e event)
 {
-    //printf("\nspi_event_cb_fun:%d\n",event);
+    // printf("\nspi_event_cb_fun:%d\n",event);
 }
 
 void example_pin_spi_init(void)
@@ -44,6 +44,15 @@ void example_pin_spi_init(void)
     drv_pinmux_config(EXAMPLE_PIN_SPI_MOSI, EXAMPLE_PIN_SPI_MOSI_FUNC);
     drv_pinmux_config(EXAMPLE_PIN_SPI_SCK, EXAMPLE_PIN_SPI_SCK_FUNC);
     drv_pinmux_config(EXAMPLE_PIN_SPI_CS, EXAMPLE_PIN_SPI_CS_FUNC);
+}
+
+static void test_sleep(uint32_t k)
+{
+    uint32_t i, j;
+
+    for (i = 0; i < 100000000; i++) {
+        for (j = 0; j < k; j++);
+    }
 }
 
 static int wujian100_spi_init(int32_t idx)//idx->MY_USI_IDX
@@ -63,48 +72,75 @@ static int wujian100_spi_init(int32_t idx)//idx->MY_USI_IDX
     ret = csi_spi_config(spi_t, MY_SPI_CLK_RATE, SPI_MODE_MASTER,
         SPI_FORMAT_CPOL0_CPHA0, SPI_ORDER_MSB2LSB,
         SPI_SS_MASTER_SW, 8);
-		
-    //MY_SPI_CLK_RATE not defined
 
     if (ret != 0) {
         printf("%s(), %d spi config error, %d\n", __func__, __LINE__, ret);
         return -1;
     }
-	
+
 	return 0;
+}
+
+
+
+static void wujian100_spi_test(void *args){
+    printf("start FPGA spi test.\n");
+    spi_handle_t handle = spi_t;
+    uint8_t data_test = 100;
+    int32_t ret;
+    while(1){
+		test_sleep(1000000);
+        csi_spi_ss_control(handle, SPI_SS_ACTIVE);
+		
+//		printf("recv:%d\n", data_test);
+//        ret=csi_spi_receive(spi_t, &data_test, 1);
+//        if(ret<0){
+//            printf("recv failed.\n");
+//        }
+        
+		printf("send:%d\n", data_test);
+	    ret=csi_spi_send(spi_t, &data_test, 1);
+	    if(ret<0){
+		   printf("send failed.\n");
+	    }
+        csi_spi_ss_control(handle, SPI_SS_INACTIVE);
+		data_test--;
+        
+    }
 }
 
 int main(void)
 {
-	int p[100];//test data
-    for (int j = 0; j < 100; j++)
-    {
-        p[j] = j;
-    }
-	uint32_t timecount = 0;
-	uint32_t i=0;
+	// int p[100];//test data
+    // for (int j = 0; j < 100; j++)
+    // {
+    //     p[j] = j;
+    // }
+	// uint32_t timecount = 0;
+	// uint32_t i=0;
 	wujian100_spi_init(MY_USI_IDX);
-	printf("start sending msg.\n");
+
+    wujian100_spi_test(0);
+
+    // while(1)
+	// {
+	// spi_handle_t handle = spi_t;
+    // csi_spi_ss_control(handle, SPI_SS_INACTIVE);
+    // csi_spi_ss_control(handle, SPI_SS_ACTIVE);
 	
-	while(1)
-	{
-	spi_handle_t handle = spi_t;
-    csi_spi_ss_control(handle, SPI_SS_INACTIVE);
-    csi_spi_ss_control(handle, SPI_SS_ACTIVE);
-	
-    csi_spi_send(handle, p+i, 1);//p->register storing data,1->one byte
-    while (csi_spi_get_status(handle).busy)
-    {
-        timecount++;
-		printf("timeout: %d\n\r",timecount);
-        if (timecount >= TEST_SPI_TIMEOUT)//TEST_SPI_TIMEOUT not defined
-        {
-            break;
-        }
-    }
-    csi_spi_ss_control(handle, SPI_SS_INACTIVE);
-    timecount = 0;
-	i=i+1;
-	if(i==100) i=0;
-	}
+    // csi_spi_send(handle, p+i, 1);//p->register storing data,1->one byte
+    // while (csi_spi_get_status(handle).busy)
+    // {
+    //     timecount++;
+	// 	printf("timeout: %d\n",timecount);
+    //     if (timecount >= TEST_SPI_TIMEOUT)//TEST_SPI_TIMEOUT not defined
+    //     {
+    //         break;
+    //     }
+    // }
+    // csi_spi_ss_control(handle, SPI_SS_INACTIVE);
+    // timecount = 0;
+	// i=i+1;
+	// if(i==100) i=0;
+	// }
 }
