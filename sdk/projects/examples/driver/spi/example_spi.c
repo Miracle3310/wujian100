069f22fe2be6e29fceadc9d8e2754e686c45a85b
miracle3310 <miracle3310@qq.com>
2021-07-23 05:55:14 +0800

    static data_test for larger spi trans

--- sdk/projects/examples/driver/spi/example_spi.c
+++ sdk/projects/examples/driver/spi/example_spi.c
@@ -25,11 +25,11 @@
 #define NCHANNEL 1
 #define TOTALBYTE (LENGTH*LENGTH*NCHANNEL)
 
- extern int32_t w25q64flash_read_id(spi_handle_t handle, uint32_t* id_num);
- extern int32_t w25q64flash_erase_sector(spi_handle_t handle, uint32_t addr);
- extern int32_t w25q64flash_erase_chip(spi_handle_t handle);
- extern int32_t w25q64flash_program_data(spi_handle_t handle, uint32_t addr, const void* data, uint32_t cnt);
- extern int32_t w25q64flash_read_data(spi_handle_t handle, uint32_t addr, void* data, uint32_t cnt);
+//  extern int32_t w25q64flash_read_id(spi_handle_t handle, uint32_t* id_num);
+//  extern int32_t w25q64flash_erase_sector(spi_handle_t handle, uint32_t addr);
+//  extern int32_t w25q64flash_erase_chip(spi_handle_t handle);
+//  extern int32_t w25q64flash_program_data(spi_handle_t handle, uint32_t addr, const void* data, uint32_t cnt);
+//  extern int32_t w25q64flash_read_data(spi_handle_t handle, uint32_t addr, void* data, uint32_t cnt);
 extern int32_t drv_pinmux_config(pin_name_e pin, pin_func_e pin_func);
 
 static spi_handle_t spi_t;
@@ -92,14 +92,14 @@ static void wujian100_spi_test(void *args){
     printf("start FPGA spi test.\n");
     spi_handle_t handle = spi_t;
     // ElementType data_test = 200;
-    ElementType data_test[NBYTE] = {0};
+    static ElementType data_test[NBYTE] = {0};
 //    ElementType nbyte = NBYTE;
-    uint32_t frame_num = 1;
+    uint32_t frame_num = 0;
 //	ElementType request[NBYTE]={0};
 //   	ElementType ack[NBYTE+1]={0};
     int32_t ret;
 	uint32_t i,j;
-    ElementType begin = 200;
+    ElementType begin = 255;
     ElementType end = 0;
 
     // initial
@@ -122,32 +122,36 @@ static void wujian100_spi_test(void *args){
         for (i = 0; i < LENGTH; i++)
         {
             data_test[i * LENGTH+frame_num] = begin;
-            data_test[i * LENGTH+frame_num-1] = end;
+
+            if(frame_num==0){
+                data_test[i * NBYTE + LENGTH - 1] = end;
+            }
+            else{
+                data_test[i * LENGTH + LENGTH - 1] = end;
+            }
         }
         frame_num++;
         if (frame_num == LENGTH)
         {
-            frame_num = 1;
+            frame_num = 0;
         }
+
         // send
+	    csi_spi_ss_control(handle, SPI_SS_ACTIVE);
         for (j = 0; j < (TOTALBYTE / NBYTE); j++)
         {
             print_data(data_test, NBYTE);
-
-//            mdelay(100);
-            //			csi_spi_ss_control(handle, SPI_SS_ACTIVE);
-            //			mdelay(50);
+            // mdelay(100);
+            mdelay(50);
             ret = csi_spi_send(spi_t, data_test, NBYTE + 1);
             //			while (csi_spi_get_status(handle).busy);
-            //			csi_spi_ss_control(handle, SPI_SS_INACTIVE);
 		}
-		mdelay(100);
-//		printf("test\r\n");
+		// mdelay(100);
+		// printf("test\r\n");
 		csi_spi_ss_control(handle, SPI_SS_INACTIVE);
-//		mdelay(50);
+		mdelay(50);
         }
 }
-
 
 
 int main(void)
@@ -155,26 +159,4 @@ int main(void)
 //	printf("test");
 	wujian100_spi_init(MY_USI_IDX);
     wujian100_spi_test(0);
-
-    // while(1)
-	// {
-	// spi_handle_t handle = spi_t;
-    // csi_spi_ss_control(handle, SPI_SS_INACTIVE);
-    // csi_spi_ss_control(handle, SPI_SS_ACTIVE);
-	
-    // csi_spi_send(handle, p+i, 1);//p->register storing data,1->one byte
-    // while (csi_spi_get_status(handle).busy)
-    // {
-    //     timecount++;
-	// 	printf("timeout: %d\n",timecount);
-    //     if (timecount >= TEST_SPI_TIMEOUT)//TEST_SPI_TIMEOUT not defined
-    //     {
-    //         break;
-    //     }
-    // }
-    // csi_spi_ss_control(handle, SPI_SS_INACTIVE);
-    // timecount = 0;
-	// i=i+1;
-	// if(i==100) i=0;
-	// }
 }
